0cdb050d074d84949508418cdcfd17483f52d792
Alex McGuire <cage433@gmail.com>
2011-09-09 07:06:52 +0100

    refactor project and start starling spike

--- src/maker/project/Project.scala
+++ src/maker/project/Project.scala
@@ -20,37 +20,25 @@ object Project{
 
   private def jar : String = java_home + "/bin/jar"
 
-
-
-  def findFiles(pred : File => Boolean, dirs : File*) : List[File] = {
-    def rec(file : File) : List[File] = {
-      if (file.isDirectory)
-        file.listFiles.toList.flatMap(rec)
-      else if (pred(file))
-        List(file)
-      else
-        Nil
-    }
-    dirs.toList.flatMap(rec)
-  }
-
 }
 
 case class Project(
   name : String,
   root : File,
   srcDirs : List[File],
-  jars : List[File],
+  jarDirs : List[File],
   outputDir : File,
   packageDir : File,
   dependencies : List[Project] = Nil
 ){
   import Project._
+  import maker.utils.FileUtils._
 
   def dependsOn(projects : Project *) = copy(dependencies = dependencies ::: projects.toList)
 
-  def srcFiles = findFiles({f : File => f.getName.endsWith(".scala")}, srcDirs : _*)
-  def classFiles = findFiles({f : File => f.getName.endsWith(".class")}, outputDir)
+  def srcFiles = findSourceFiles(srcDirs : _*)
+  def classFiles = findClasses(outputDir)
+  def jars = findJars(jarDirs : _*)
   def classpath = (outputDir :: jars).map(_.getAbsolutePath).mkString(":")
   def outputJar = new File(packageDir.getAbsolutePath, name + ".jar")
 
