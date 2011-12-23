d9449b5a18a9887a0586e5dd3873438cb5c0ec8f
LouisB <chillipower.uk@gmail.com>
2011-12-23 13:20:26 +0000

    small update to try and get initialisation more stable

--- Maker.scala
+++ Maker.scala
@@ -1,19 +1,8 @@
 import maker.project.Project
-import java.io.File
-import org.apache.commons.io.{FileUtils => ApacheFileUtils}
 
 System.setProperty("scala.usejavacp", "false")
 
-def standardProject(name : String) : Project = Project(
-  name, 
-  new File(name), 
-  List(new File(name + "/src")), 
-  List(new File(name + "/tests")), 
-  List(new File("./lib")),
-  Nil
-)
-
-lazy val utils = standardProject("utils")
-lazy val plugin = standardProject("plugin") dependsOn utils
-lazy val maker = standardProject("maker") dependsOn plugin
+lazy val utils = Project("utils")
+lazy val plugin = Project("plugin") dependsOn utils
+lazy val maker = Project("maker") dependsOn plugin
 
--- bin/maker.sh
+++ bin/maker.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-fsc -reset
+# belt and braces initial state to prevent any funny stuff!
+rm -rf out
+export CLASSPATH=
+fsc -shutdown
 
 ./bin/build-from-scratch.rb
 ./bin/write-classpath.rb
 
 . ./set-maker-classpath.sh
-
 . ./bin/set-opts.sh
 
 fsc -reset
 
-$SCALA_HOME/bin/scala -Yrepl-sync -i Maker.scala -e "maker compile"
+#$SCALA_HOME/bin/scala -nc -Yrepl-sync -i Maker.scala -e "maker compile"
+$SCALA_HOME/bin/scala -nc -Yrepl-sync -i Maker.scala
 
 . ./set-classpath.sh
 
