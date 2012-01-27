6873fb16d19fa0e0e4a15d19116a0b2434ecb4e4
LouisB <chillipower.uk@gmail.com>
2012-01-27 19:28:38 +0000

    fix bootstrap.sh to use MAKER_BUILD_MEM if set

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -11,13 +11,14 @@ rm -rf out
 export CLASSPATH=
 
 # Set java heap size to something nice and big
-if [ -z MAKER_BUILD_MEM ];
+if [ -z $MAKER_BUILD_MEM ];
 then
-  mem=$MAKER_BUILD_MEM
-else
   totalMem=$(cat /proc/meminfo | head -n 1 | awk '/[0-9]/ {print $2}')
   mem=$[$totalMem/1024/4]
+else
+  mem=$MAKER_BUILD_MEM
 fi
+
 if [ $mem -lt 1024 ];
 then
   echo "Memory may be too low - try setting MAKER_BUILD_MEM to at least 2000"
@@ -39,5 +40,6 @@ $SCALA_HOME/bin/scala -nc -Yrepl-sync -i Maker.scala -e "{ val m =  makerProj; m
 
 ./bin/write-classpath.rb
 
-
+echo "Maker built successfully"
+echo "Note, use '. set-maker-classpath.sh' to set up the classpath for maker in the environment of this shell"
 
