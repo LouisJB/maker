aeada10bb0e403bc2e5c4f60bb182a1b04e48b65
LouisB <chillipower.uk@gmail.com>
2012-02-02 09:57:23 +0000

    update to filter null compile java tasks from project task dependencies

--- maker/src/maker/graphviz/Graphviz.scala
+++ maker/src/maker/graphviz/Graphviz.scala
@@ -3,32 +3,28 @@ package maker.graphviz
 import maker.project.Project
 import maker.os.Command
 import maker.utils.os.OsUtils._
-import java.io.File
 import maker.task.{CompileJavaSourceTask, ProjectAndTask}
-
+import java.io.File
 
 object GraphVizDiGrapher {
-  val tmpImageFileName = "Maker-Project-Graph"
+  val graphName = "Maker-Project-Graph"
+
+  def mkGraph(name : String,  graphDef : String) = {
+    val g = "digraph \\\"%s\\\" { graph[size=\\\"100.0, 100.0\\\"]; "
+    (g + "{ node[ratio=compress size = \\\"100.0, 100.0\\\" nodesep=\\\"0.1\\\" ranksep=\\\"0.1\\\" fontname=Helvetica fontsize=8]; %s ;} ;}")
+      .format(name, graphDef)
+  }
 
   def makeDot(graph : List[(Project, List[Project])], showLibDirs : Boolean = false, showLibs : Boolean = false) : String = {
-    
     def mkProjectDep(p1 : Project,  p2 : Project) =
       "{ node[fillcolor=yellow style=filled]; \\\"%s\\\"->\\\"%s\\\" ;}".format(p1.name, p2.name)
 
     def mkLibDirDep(p : Project,  d : File) =
       "\\\"%s\\\"->{ node[shape=box color=blue style=filled fillcolor=lightskyblue]; \\\"%s\\\" ;}".format(p.name, d.getPath)
-      //"\\\"%s\\\"->{ node [shape=polygon sides=4 skew=0.2 color=red];\\\"%s\\\";}".format(p.name, d.getPath)
 
     def mkLibFileDep(d : File, df : File) =
       "\\\"%s\\\"->{ node[color=none; shape=plaintext]; \\\"%s\\\" ;}".format(d.getPath, df.getName())
 
-    def mkGraph(name : String,  graphDef : String) = {
-      val g = "digraph \\\"%s\\\" { graph[size=\\\"100.0, 100.0\\\"]; "
-      (g + "{ node[ratio=compress size = \\\"100.0, 100.0\\\" nodesep=\\\"0.1\\\" ranksep=\\\"0.1\\\" fontname=Helvetica fontsize=8]; %s ;} ;}")
-        .format(name, graphDef)
-    }
-
-    // take the distinct set here as we want to ignore duplicated paths caused by indirect dependencies
     val g = graph.distinct.flatMap{ case (proj, deps) => deps.flatMap(pd => {
         mkProjectDep(proj, pd) :: { if (showLibDirs)
               proj.libDirs.flatMap(libDir => mkLibDirDep(proj, libDir) :: { if (showLibs)
@@ -39,7 +35,7 @@ object GraphVizDiGrapher {
         }
       })
     }
-    val dot = mkGraph(tmpImageFileName, g.distinct.mkString(" "))
+    val dot = mkGraph(graphName, g.distinct.mkString(" "))
     println("dot = " + dot)
     dot
   }
@@ -51,12 +47,12 @@ object GraphVizDiGrapher {
     def ptLabel(pt : ProjectAndTask) = {
       val size = pt.lastRunTimeMs.toDouble / avgTime
       val nodeAttrs = if (pt.task == CompileJavaSourceTask) " style=filled fillcolor=lightskyblue" else ""
-      "{ \\\"<%s>\\\n%s\\\nTook %dms \\\" [width=%f height=%f %s] }".format(pt.task, pt.project.name, pt.lastRunTimeMs, size*2.0, size, nodeAttrs)
+      "{ \\\"<%s> %s Took %dms\\\" [width=%f height=%f %s] }".format(pt.task, pt.project.name, pt.lastRunTimeMs, size*2.0, size, nodeAttrs)
     }
     val g = ps.distinct.flatMap(pt => pt._2.map(p => {
       "%s->%s".format(ptLabel(pt._1), ptLabel(p))
-    })).mkString(" ")
-    val dot = "digraph \\\"" + tmpImageFileName + "\\\" { %s }".format(g)
+    }))
+    val dot = mkGraph(graphName, g.distinct.mkString(" "))
     println("dot = " + dot)
     dot
   }
@@ -64,7 +60,7 @@ object GraphVizDiGrapher {
   def makeDotFromString(graph : List[(Project, List[String])]) : String = {
     val g = graph.distinct.flatMap(pd => pd._2.map(p =>
           "\\\"-Project-%s\\\"->\\\"%s\\\"".format(pd._1.name, p))).mkString(" ")
-    val dot = "digraph \\\"" + tmpImageFileName + "\\\" { %s }".format(g)
+    val dot = mkGraph(graphName, g.distinct.mkString(" "))
     println("dot = " + dot)
     dot
   }
@@ -73,17 +69,20 @@ object GraphVizDiGrapher {
 object GraphVizUtils {
   val DEFAULT_IMAGE_FORMAT = "png"
   val DEFAULT_TMP_FILE = new File("maker-gv-tmp." + DEFAULT_IMAGE_FORMAT)
-  def createDotFile(graphDef : String, file : java.io.File = DEFAULT_TMP_FILE) = {
+  def removeGraphFile(file : File = DEFAULT_TMP_FILE) = {
+    Command("rm", file.getAbsolutePath).exec()
+    file
+  }
+  def createGraphFile(graphDef : String, file : File = DEFAULT_TMP_FILE) = {
     Command("/bin/sh", "-c", "echo \"" + graphDef + "\" | dot -T" + DEFAULT_IMAGE_FORMAT + " > " + file.getAbsolutePath).exec()
     file
   }
-
-  def showGraph(graphDef : String, file : java.io.File = DEFAULT_TMP_FILE) {
-    val f = createDotFile(graphDef, file)
+  def showGraph(graphDef : String, file : File = DEFAULT_TMP_FILE) {
+    removeGraphFile(file)
+    val f = createGraphFile(graphDef, file)
     if (isLinux)
       Command("xdg-open", f.getAbsolutePath).exec(true)
-    else // assume OSX unless we want to support other OSes such as windows
+    else // assume OSX until we want to support other OSes such as windows
       Command("open", f.getAbsolutePath).exec()
   }
 }
-
