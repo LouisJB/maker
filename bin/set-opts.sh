044a5976b006f1cb944c600c15688f3eed26a865
Alex McGuire <alex.mcguire@trafigura.com>
2012-02-19 22:25:56 +0000

    Change README to reflect latest script and remove redundant scripts

--- bin/set-opts.sh
+++
@@ -1,4 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mem=$(cat /proc/meminfo | head -n 1 | awk '/[0-9]/ {print $2}')
-
-export JAVA_OPTS="-Xmx$[$mem/1020/4]m -Xms$[$mem/1020/4/10]m -javaagent:$DIR/../lib/jrebel.jar -noverify"
