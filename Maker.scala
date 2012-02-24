b773900582c0b3dbfeb2f21d5686f08ec820419d
LouisB <chillipower.uk@gmail.com>
2012-02-24 12:05:59 +0000

    update to maker file for new maker lib location

--- Maker.scala
+++ Maker.scala
@@ -8,7 +8,7 @@ System.setProperty("scala.usejavacp", "false")
 System.setProperty("log4j.ignoreTCL", "true")
 
 val props = Props(file("Maker.conf"))
-lazy val libDirs = List(file("maker-lib"))
+lazy val libDirs = List(file(".maker/lib"))
 
 lazy val utils = Project("utils", file("utils"), libDirs = libDirs, props = props)
 lazy val plugin = Project("plugin", file("plugin"), libDirs = libDirs, props = props) dependsOn utils
