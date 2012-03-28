5b9dddf3aea136faa1da4e7adec661f57cfe89b6
LouisB <chillipower.uk@gmail.com>
2012-03-28 21:31:39 +0100

    minor tidy of command utils

--- maker/src/maker/os/Command.scala
+++ maker/src/maker/os/Command.scala
@@ -1,15 +1,15 @@
 package maker.os
 
+import maker.utils.{TeeToFileOutputStream, Log}
 import java.lang.ProcessBuilder
 import java.io.{File, OutputStream, InputStreamReader, BufferedReader, PrintWriter}
-import maker.utils.{TeeToFileOutputStream, Log}
 import actors.Future
 import actors.Futures._
 
 
-case class Command(os : OutputStream, args : String*) {
+case class Command(os : OutputStream, closeStream : Boolean, args : String*) {
 
-  private def startProc() : Process = {
+  def startProc() : Process = {
     val procBuilder = new ProcessBuilder(args : _*)
     procBuilder.redirectErrorStream(true)
     procBuilder.start
@@ -17,19 +17,8 @@ case class Command(os : OutputStream, args : String*) {
 
   def execProc() : (Process, Future[(Int,  String)]) = {
     val proc = startProc()
-    val isr = new InputStreamReader(proc.getInputStream)
-    val br = new BufferedReader(isr)
     (proc, future {
-      val buf = new StringBuffer()
-      val ps = new PrintWriter(os, true)
-      var line : String = null
-      line = br.readLine()
-      while (line != null) {
-        ps.println(line)
-        line = br.readLine()
-        buf.append(line)
-      }
-      (proc.waitFor, buf.toString)
+      waitFor(os, closeStream, proc)
     })
   }
 
@@ -43,42 +32,34 @@ case class Command(os : OutputStream, args : String*) {
     else (0, "")
   }
 
-  // keeping this around just in case it's useful, should be replaced with the equivalent above so will remove shortly anyway
-  @deprecated
-  def execInline(async : Boolean = false) : (Int, String) = {
-    Log.debug("Executing cmd (async = " + async + ") - " + toString)
-    val procBuilder = new ProcessBuilder(args : _*)
-    procBuilder.redirectErrorStream(true)
-    val proc = procBuilder.start
-    val (procResult, msg) = if (!async) {
-      var ps : PrintWriter = null
-      val buf = new StringBuffer()
-      try {
-        ps = new PrintWriter(os)
-        val isr = new InputStreamReader(proc.getInputStream)
-        val br = new BufferedReader(isr)
-        var line : String =null;
+  def waitFor(os : OutputStream, closeStream : Boolean, proc : Process) = {
+    val buf = new StringBuffer()
+    var ps : PrintWriter = null
+    try {
+      val br = new BufferedReader(new InputStreamReader(proc.getInputStream))
+      ps = new PrintWriter(os, true)
+      var line : String = null
+      line = br.readLine()
+      while (line != null) {
+        ps.println(line)
         line = br.readLine()
-        while (line != null) {
-          ps.println(line)
-          line = br.readLine()
-          buf.append(line)
-        }
+        buf.append(line)
       }
-      finally {
-        ps.flush()
-        ps.close
-      }
-      (proc.waitFor, buf.toString)
     }
-    else (0, "")
-    (procResult, msg)
+    finally {
+      if (ps != null) {
+        ps.flush
+        if (closeStream) ps.close()
+      }
+    }
+    (proc.waitFor, buf.toString)
   }
 
   override def toString = "Command: " + args.mkString(" ")
 }
 
 object Command{
-  def apply(args : String*) : Command = Command(System.out, args : _*)
-  def apply(file : File, args : String*) : Command = Command(TeeToFileOutputStream(file), args : _*)
+  def apply(os : OutputStream, args : String*) : Command = Command(os, false, args : _*)
+  def apply(args : String*) : Command = Command(System.out, false, args : _*)
+  def apply(file : File, args : String*) : Command = Command(TeeToFileOutputStream(file), true, args : _*)
 }
