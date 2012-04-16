3dfe7aa4603733e4dbd69d118588c91f8cd5a55d
LouisB <chillipower.uk@gmail.com>
2012-04-16 07:53:53 +0100

    improve representation of a maven/ivy dependency

--- utils/src/maker/utils/Dependencies.scala
+++ utils/src/maker/utils/Dependencies.scala
@@ -3,28 +3,50 @@ package maker.utils
 import org.apache.ivy.ant.IvyMakePom
 import org.apache.ivy.plugins.parser.m2.PomWriterOptions
 
+// mvn group -> artifact -> version <-> ivy org -> name -> rev
+case class GroupId(id : String) {
+  def %(artifactId : String) = GroupAndArtifact(this, ArtifactId(artifactId))
+}
+object GroupId {
+  implicit def toGroupId(id : String) : GroupId = new GroupId(id)
+}
+case class ArtifactId(id : String)
+trait GAV {
+  val groupId : GroupId
+  val artifactId : ArtifactId
+  val version : Option[Version] = None
+}
+case class GroupAndArtifact(groupId : GroupId, artifactId : ArtifactId) extends GAV {
+  def %(version : String) = GroupArtifactAndVersion(groupId, artifactId, Some(Version(version)))
+}
+case class Version(version : String)
+case class GroupArtifactAndVersion(groupId : GroupId, artifactId : ArtifactId, override val version : Option[Version]) extends GAV
 
 /**
  * Simple case class representing a library dependency definition (from a Maven repo)
  */
 case class DependencyLib(
     name : String,
+    gav : GroupArtifactAndVersion,
+    /*
     groupId : String,
     artifactId : String,
     version : String,
     org : String,
+    */
     scope : String,
     classifierOpt : Option[String] = None) {
 
   val classifier = classifierOpt.getOrElse("")
+  val version = gav.version.map(_.version).getOrElse("")
 
-  override def toString = "Name: %s Version: %s Org: %s Classifier: %s".format(name, version, org, classifier)
+  override def toString = "Name: %s Version: %s Org: %s Classifier: %s".format(name, gav, classifier)
 
   def toIvyMavenDependency : IvyMakePom#Dependency = {
     val ivyMakePom : IvyMakePom = new IvyMakePom
     val dep = new ivyMakePom.Dependency()
-    dep.setGroup(groupId)
-    dep.setArtifact(artifactId)
+    dep.setGroup(gav.groupId.id)
+    dep.setArtifact(gav.artifactId.id)
     dep.setVersion(version)
     dep.setScope(scope)
     dep.setOptional(false)
@@ -32,7 +54,7 @@ case class DependencyLib(
   }
 
   def toIvyPomWriterExtraDependencies : PomWriterOptions.ExtraDependency =
-    new PomWriterOptions.ExtraDependency(groupId, artifactId, version, scope, false)
+    new PomWriterOptions.ExtraDependency(gav.groupId.id, gav.artifactId.id, version, scope, false)
 }
 
 object DependencyLib {
