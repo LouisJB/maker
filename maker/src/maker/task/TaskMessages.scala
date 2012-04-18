3f769b03cfe725631dd3e6504329e95516d9de75
LouisB <chillipower.uk@gmail.com>
2012-04-18 20:49:54 +0100

    refactor

---
+++ maker/src/maker/task/TaskMessages.scala
@@ -0,0 +1,57 @@
+package maker.task
+
+import akka.actor.Actor
+import java.io.File
+import maker.utils.Log
+
+trait TaskResult
+
+case object TaskSuccess extends TaskResult {
+  override def toString = "OK"
+}
+case class TaskError(reason : String, exception : Option[Throwable]) extends TaskResult {
+  override def toString = "Task error, reason: " + reason + exception.map(e => ", exception " + e.getMessage).getOrElse("")
+}
+
+sealed trait BuildMessage
+
+case class ExecTaskMessage(projectTask : ProjectAndTask,
+                           acc : Map[Task, List[AnyRef]],
+                           parameters : Map[String, String] = Map()) extends BuildMessage
+
+case class TaskResultMessage(projectTask : ProjectAndTask, result : Either[TaskFailed, AnyRef]) extends BuildMessage
+
+case object StartBuild extends BuildMessage
+
+class Worker() extends Actor{
+  def receive = {
+    case ExecTaskMessage(projectTask : ProjectAndTask, acc : Map[Task, List[AnyRef]], parameters : Map[String, String]) => self reply {
+      try {
+        TaskResultMessage(projectTask, projectTask.exec(acc, parameters))
+      }
+      catch {
+        case e =>
+          TaskResultMessage(projectTask, Left(TaskFailed(projectTask, e.getMessage)))
+      }
+    }
+  }
+}
+
+case class BuildResult(res : Either[TaskFailed, AnyRef],
+                       projectAndTasks : Set[ProjectAndTask],
+                       originalProjectAndTask : ProjectAndTask) extends BuildMessage {
+  import maker.graphviz.GraphVizDiGrapher._
+  import maker.graphviz.GraphVizUtils._
+  def stats = projectAndTasks.map(_.allStats).mkString("\n")
+
+  def resultTree(pt : ProjectAndTask = originalProjectAndTask) =
+    pt.getTaskTree.map(p => (projectAndTasks.find(_ == p._1).get, p._2.map(pt => projectAndTasks.find(_ == pt).get)))
+
+  def showBuildGraph() : Option[File] =
+    resultTree() match {
+      case Nil | null => Log.info("No project results to graph"); None
+      case r => Some(showGraph(makeDotFromProjectAndTask(r)))
+    }
+
+  override def toString = res.toString
+}
