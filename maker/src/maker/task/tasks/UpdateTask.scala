e81a532c90c4e5e3e2deb84ccff16d92d160fe45
Louis Botterill <louis.botterill@trafigura.com>
2012-04-24 15:55:49 +0100

    fix mistake in update task

--- maker/src/maker/task/tasks/UpdateTask.scala
+++ maker/src/maker/task/tasks/UpdateTask.scala
@@ -12,51 +12,44 @@ import maker.task.{ProjectAndTask, TaskFailed, Task}
 
 case object UpdateTask extends Task {
   def exec(project: Project, acc: List[AnyRef], parameters: Map[String, String] = Map()) = {
-    Log.debug("project.props.UpdateOnCompile() = " + project.props.UpdateOnCompile())
-    if (project.props.UpdateOnCompile()) {
-      try {
-        if (project.ivyFile.exists) {
-          val confs = Array[String]("default")
-          val artifactFilter = FilterHelper.getArtifactTypeFilter(Array[String]("jar", "war", "bundle", "source"))
-          val resolveOptions = new ResolveOptions().setConfs(confs)
-            .setValidate(true)
-            .setArtifactFilter(artifactFilter)
-          val ivy = Ivy.newInstance
-          val settings = ivy.getSettings
-          settings.addAllVariables(System.getProperties)
-          ivy.configure(project.ivySettingsFile)
-          val ivyFile = file(project.root, nameAndExt(project.ivyFile)._1 + "-dynamic.ivy")
-          copyFile(project.ivyFile, ivyFile)
+    try {
+      if (project.ivyFile.exists) {
+        val confs = Array[String]("default")
+        val artifactFilter = FilterHelper.getArtifactTypeFilter(Array[String]("jar", "war", "bundle", "source"))
+        val resolveOptions = new ResolveOptions().setConfs(confs)
+          .setValidate(true)
+          .setArtifactFilter(artifactFilter)
+        val ivy = Ivy.newInstance
+        val settings = ivy.getSettings
+        settings.addAllVariables(System.getProperties)
+        ivy.configure(project.ivySettingsFile)
+        val ivyFile = file(project.root, nameAndExt(project.ivyFile)._1 + "-dynamic.ivy")
+        copyFile(project.ivyFile, ivyFile)
 
-          val excludedBinaryModules : List[GroupAndArtifact] = project.allDeps.map(_.moduleId) ::: project.allDeps.flatMap(_.additionalExcludedLibs.map(_.toGroupAndArtifact))
-          val excludes = excludedBinaryModules.map(e => <exclude org={e.groupId.id} module={e.artifactId.id} />.toString)
+        val excludedBinaryModules : List[GroupAndArtifact] = project.allDeps.map(_.moduleId) ::: project.allDeps.flatMap(_.additionalExcludedLibs.map(_.toGroupAndArtifact))
+        val excludes = excludedBinaryModules.map(e => <exclude org={e.groupId.id} module={e.artifactId.id} />.toString)
 
-          replaceInFile(ivyFile, "${maker.module.excluded.libs}", excludes.mkString("\n"))
-          ivy.configure(ivyFile)
+        replaceInFile(ivyFile, "${maker.module.excluded.libs}", excludes.mkString("\n"))
+        ivy.configure(ivyFile)
 
-          val report = ivy.resolve(ivyFile.toURI().toURL(), resolveOptions)
-          val md = report.getModuleDescriptor
-          ivy.retrieve(
-            md.getModuleRevisionId(),
-            project.managedLibDir.getPath + "/[artifact]-[revision](-[classifier]).[ext]",
-            new RetrieveOptions()
-              .setConfs(confs).setSync(true)
-              .setArtifactFilter(artifactFilter))
-          Right("OK")
-        } else {
-          Log.info("Nothing to update")
-          Right("OK")
-        }
-      }
-      catch {
-        case e =>
-          e.printStackTrace
-          Left(TaskFailed(ProjectAndTask(project, this), e.getMessage))
+        val report = ivy.resolve(ivyFile.toURI().toURL(), resolveOptions)
+        val md = report.getModuleDescriptor
+        ivy.retrieve(
+          md.getModuleRevisionId(),
+          project.managedLibDir.getPath + "/[artifact]-[revision](-[classifier]).[ext]",
+          new RetrieveOptions()
+            .setConfs(confs).setSync(true)
+            .setArtifactFilter(artifactFilter))
+        Right("OK")
+      } else {
+        Log.info("Nothing to update")
+        Right("OK")
       }
     }
-    else {
-      Log.debug("Auto update disabled, skipping")
-      Right("OK")
+    catch {
+      case e =>
+        e.printStackTrace
+        Left(TaskFailed(ProjectAndTask(project, this), e.getMessage))
     }
   }
 }
