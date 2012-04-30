4273f3db4897040686f700387eb2cde3ad608b5f
Alex McGuire <cage433@gmail.com>
2012-04-30 23:54:23 +0100

    Let maker reference itself as a project
    Makes it easy to get a classpath to run remote tasks with. May need to
    not be global

---
+++ maker/src/maker/Maker.scala
@@ -0,0 +1,27 @@
+package maker
+
+import maker.project.Project
+import maker.utils.FileUtils._
+import maker.utils.Log._
+import java.io.File
+
+object Maker{
+  val ivyFileName = "ivy.xml"
+  val ivySettingsFile_ = file("ivysettings.xml")
+  val propsFile = Props(file("Maker.conf"))
+
+  def mkProject(name : String, libs : List[File] = Nil) = new Project(
+    name, file(name),
+    libDirs=libs,
+    resourceDirs = List(file(name + "/resources")),
+    props = propsFile,
+    ivySettingsFile = ivySettingsFile_,
+    ivyFileRel = ivyFileName
+  )
+
+  lazy val utils = mkProject("utils", List(file("utils/maker-lib"), file("libs/")))
+  lazy val plugin = mkProject("plugin") dependsOn utils
+  lazy val makerProj = mkProject("maker") dependsOn plugin
+
+  lazy val mkr = makerProj
+}
